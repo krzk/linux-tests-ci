c06d034290968e3721a854459c80f69e7e82a918
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-08-19 16:00:56 +0200

    Add DTS check on PowerPC and MIPS architectures
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>

---
+++ bin/install.ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (c) 2021 Canonical Ltd.
+# Author: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
+#                             <krzk@kernel.org>
+#
+
+set -ex
+
+if [ -z "$ARCH" ]; then
+	echo "missing \$ARCH!" >&2
+	exit 1
+fi
+
+PKGS_CC=""
+
+case "$ARCH" in
+	arm) PKGS_CC="gcc-arm-linux-gnueabi";;
+	arm64) PKGS_CC="gcc-aarch64-linux-gnu";;
+	mips) PKGS_CC="gcc-${CROSS_COMPILE%-}";;
+	powerpc) PKGS_CC="gcc-powerpc-linux-gnu";;
+	*) echo "unsupported arch: '$ARCH'!" >&2; exit 1;;
+esac
+
+sudo apt update
+
+sudo apt install -y --no-install-recommends build-essential python3-pip libyaml-dev
+sudo apt install -y --no-install-recommends $PKGS_CC
+
+echo "Install finished: $0"
